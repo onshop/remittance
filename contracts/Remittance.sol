6cb622f8d0df004f915ae71fdc75a2dad20374c3
Ben Parish <ben.parish@onshop.co.uk>
2021-02-11 16:28:20 +0000

    Got main functionality and tests completed

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -12,11 +12,12 @@ contract Remittance is Ownable, Pausable {
 
     mapping(address => uint) public balances;
 
-    mapping(bytes32 => RemittanceContract) public remittanceContracts;
+    mapping(bytes32 => RemittanceContract) public contracts;
 
     struct RemittanceContract {
-        address sender;
+        address funder;
         address broker;
+        address recipient;
         uint amount;
         bool fundsReleased;
     }
@@ -27,14 +28,15 @@ contract Remittance is Ownable, Pausable {
     );
 
     event RemittanceContractCreated(
-        address indexed sender,
-        address indexed broker,
         bytes32 hash,
+        address indexed funder,
+        address indexed broker,
+        address indexed recipient,
         uint amount
     );
 
     event WithDraw(
-        address indexed withdrawer,
+        address indexed broker,
         bytes32 hash,
         uint amount
     );
@@ -53,43 +55,54 @@ contract Remittance is Ownable, Pausable {
         emit Deposit(msg.sender, msg.value);
     }
 
-    function create(address broker, bytes32 hash, uint256 amount) external whenNotPaused returns(bool success) {
+    function create(bytes32 hash, address broker, address recipient, uint256 amount) external whenNotPaused returns(bool success) {
+        require(hash.length > 0, "The hash cannot be empty");
+        uint256 funderBalance = balances[msg.sender];
+        require(funderBalance >= amount, "There are insufficient funds in the sender's account to create this contract");
 
-        uint256 withdrawerBalance = balances[msg.sender];
-        require(withdrawerBalance >= amount, "There are insufficient funds in the sender's account to create this contract");
-
-        require(broker == address(broker) && broker != address(0));
-        require(amount > 0, "The value must be greater than 0");
+        require(broker == address(broker) && broker != address(0), "Invalid broker address");
+        require(recipient == address(recipient) && recipient != address(0), "Invalid recipient address");
+        require(broker != recipient, "The broker and recipient addresses must be different");
+        require(amount > 0, "The amount must be greater than 0");
         require(hash.length > 0, "The hash cannot be empty");
 
-        RemittanceContract storage remittanceContract = remittanceContracts[hash];
+        balances[msg.sender] = funderBalance.sub(amount);
+        balances[broker] =  balances[broker].add(amount);
 
+        RemittanceContract storage remittanceContract = contracts[hash];
+
+        remittanceContract.funder = msg.sender;
         remittanceContract.broker = broker;
+        remittanceContract.recipient = recipient;
         remittanceContract.amount = amount;
         remittanceContract.fundsReleased = false;
 
-        emit RemittanceContractCreated(msg.sender, broker, hash, amount);
+        emit RemittanceContractCreated(hash, msg.sender, broker, recipient, amount);
 
         return success;
     }
 
 
     function withdraw(string memory concatenatedPassword) public whenNotPaused returns(bool success) {
-
+        require(bytes(concatenatedPassword).length > 0, "The concatenated password cannot be empty");
         bytes32 hash = keccak256(abi.encodePacked(concatenatedPassword));
 
-        RemittanceContract storage remittanceContract = remittanceContracts[hash];
+        RemittanceContract storage remittanceContract = contracts[hash];
 
         uint amount = remittanceContract.amount;
-        address sender = remittanceContract.sender;
+        address funder = remittanceContract.funder;
 
-        uint256 withdrawerBalance = balances[sender];
-        require(withdrawerBalance >= amount, "There are insufficient funds available in the sender's account");
+        uint256 withdrawerBalance = balances[msg.sender];
+        require(withdrawerBalance >= amount, "There are insufficient funds available in the withdrawer's contract balance");
 
-        balances[sender] = SafeMath.sub(withdrawerBalance, amount);
+        uint256 funderBalance = balances[funder];
+        require(funderBalance >= amount, "There are insufficient funds available in the funder's contract balance");
+
+        balances[msg.sender] = SafeMath.sub(withdrawerBalance, amount);
         remittanceContract.fundsReleased = true;
 
         emit WithDraw(msg.sender, hash, amount);
+
         (success, ) = msg.sender.call{value: amount}("");
         require(success, "Transfer failed");
     }
--- test/remittance.js
+++ test/remittance.js
@@ -4,7 +4,88 @@ const Remittance = artifacts.require("./Remittance.sol");
 contract('Remittance', async accounts => {
 
     const { toBN } = web3.utils;
+    const concatPassword = "password1" + "password2";
 
+    const getGasCost = async txObj => {
+        const tx = await web3.eth.getTransaction(txObj.tx);
 
+        return toBN(txObj.receipt.gasUsed).mul(toBN(tx.gasPrice));
+    };
 
+    const [funder, broker, recipient] = accounts;
+    let remittance;
+``
+    beforeEach("Deploy and prepare", async function() {
+        remittance = await Remittance.new({from: funder});
+    });
+
+    it('Sender can deposit 5 wei into the broker account', async () => {
+        const txObj = await remittance.deposit({from: funder, value: 5});
+
+        truffleAssert.eventEmitted(txObj, 'Deposit', (ev) => {
+            return  ev.depositor === funder &&
+                ev.amount.toString(10) === "5";
+        }, 'Deposit event is emitted');
+
+        // Check contract's changed ETH balance
+        const contractEthBalance = toBN(await web3.eth.getBalance(remittance.address));
+        assert.strictEqual(contractEthBalance.toString(10), "5");
+    });
+
+    it('RemittanceContract is created', async () => {
+
+        await remittance.deposit({from: funder, value: toBN(5)});
+        const hash = await web3.utils.soliditySha3(concatPassword);
+        const txObj = await remittance.create(hash, broker, recipient, toBN(2), {from: funder});
+
+        // Check broker's changed contract balance
+        const brokerContractBalance = await remittance.balances(broker);
+        assert.strictEqual(brokerContractBalance.toString(10), "2");
+
+        truffleAssert.eventEmitted(txObj, 'RemittanceContractCreated', (ev) => {
+            return  ev.hash === hash &&
+                ev.funder === funder &&
+                ev.broker === broker &&
+                ev.recipient === recipient &&
+                ev.amount.toString(10) === "2";
+        }, 'RemittanceContractCreated event is emitted');
+
+        const remittanceContract = await remittance.contracts(hash);
+
+        assert.strictEqual(remittanceContract.funder, funder);
+        assert.strictEqual(remittanceContract.broker, broker);
+        assert.strictEqual(remittanceContract.recipient, recipient);
+        assert.strictEqual(remittanceContract.amount.toString(10), "2");
+        assert.strictEqual(remittanceContract.fundsReleased, false);
+    });
+
+    it('Broker withdraws eth on behalf of the recipient', async () => {
+
+        const initBrokerEthBalance = toBN(await web3.eth.getBalance(broker));
+        await remittance.deposit({from: funder, value: toBN(5)});
+        const hash = await web3.utils.soliditySha3(concatPassword);
+        await remittance.create(hash, broker, recipient, toBN(2), {from: funder});
+
+        const txObj = await remittance.withdraw(concatPassword, {from: broker});
+
+        truffleAssert.eventEmitted(txObj, 'WithDraw', (ev) => {
+            return  ev.broker === broker &&
+                ev.hash === hash &&
+                ev.amount.toString(10) === "2";
+        }, 'WithDraw event is emitted');
+
+        // Check broker's changed contract balance
+        const brokerContractBalance = await remittance.balances(broker);
+        assert.strictEqual(brokerContractBalance.toString(10), "0");
+
+        // Check broker's new Ether balance
+        const cost = toBN(await getGasCost(txObj));
+        const brokerEthBalance = toBN(await web3.eth.getBalance(broker));
+        const expectedBrokerEthBalance = initBrokerEthBalance.sub(cost).add(toBN(2)).toString(10);
+        assert.strictEqual(brokerEthBalance.toString(10), expectedBrokerEthBalance);
+
+        // Check funds released
+        const remittanceContract = await remittance.contracts(hash);
+        assert.strictEqual(remittanceContract.fundsReleased, true);
+    });
 });
